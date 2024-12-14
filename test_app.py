96459b13ba01883e4eec93f6164a0647193d09db
nirmalvarbabu <113340320+nirmalvarbabu@users.noreply.github.com>
2024-12-14 21:13:19 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,6 +5,9 @@ from app import hello_world
 class TestApp(unittest.TestCase):
     def test_hello_world(self):
         self.assertEqual(hello_world(), "Hello, World!")
+def test_say_hello():
+    assert say_hello() == "Hello, CI World!"
+
 
 if __name__ == "__main__":
     unittest.main()
